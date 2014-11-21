47e55e1b21b198cc6438d117f3e3d58f1240fd93
jayrathod <jayrathod19.19@gmail.com>
2014-11-21 10:59:43 +0530

    Create cellIdInfo.js

---
+++ cellIdInfo.js
@@ -0,0 +1,28 @@
+/**
+ * @constructor
+ */
+var cellidinfoplugin = function(){};
+
+cellidinfoplugin.prototype.getCellInfo = function(successCallback, failure){
+	cordova.exec(successCallback, failure, "CellIdInfoPlugin", "getCellInfo", []);
+};
+
+cellidinfoplugin.prototype.getGeoCords = function(successCallback, failure){
+    cordova.exec(successGeoCords, failure, "CellIdInfoPlugin", "getGeoCords", []);
+};
+
+// This callback function will be executed depending on device location i.e indoor or outdoor and may take 30 seconds to 20 minutes.
+function successGeoCords(result){
+	alert(JSON.stringify(result));
+}
+
+// Plug into Cordova
+cordova.addConstructor(function() {
+
+    if (!window.Cordova) {
+        window.Cordova = cordova;
+    };
+
+    if(!window.plugins) window.plugins = {};
+    window.plugins.CellIdInfoPlugin = new CellIdInfoPlugin();
+});
